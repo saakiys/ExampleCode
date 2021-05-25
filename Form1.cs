005e3232b82b4e35d0d2e7139860b9eaf9d9a746
saakiys <63611047+saakiys@users.noreply.github.com>
2021-05-25 06:38:21 +0300

    AdoNet

--- Form1.cs
+++ Form1.cs
@@ -7,76 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
-namespace proga
+namespace ExampleAdoNet
 {
     public partial class Form1 : Form
     {
-        BD bb = new BD();
+        //string connectionStr = "Server=localhost; Port=5432; Database=jewerly; User Id=postgres; Password=saak2108;";
         public Form1()
         {
             InitializeComponent();
+            textBox_addres.Text = "localhost";
+            textBox_port.Text = "5432";
+            textBox_user.Text = "postgres";
+            textBox_pass.Text = "saak2108";
+            textBox_database.Text = "jewerly";
+        }
+
+
+        private void btn_ConnDB_Click(object sender, EventArgs e)
+        {
+            //string connectionSt = "Server=localhost; Port=5432; Database=jewerly; User Id=postgres; Password=saak2108;";
+            string server = "Server=" + textBox_addres.Text+";";
+            string port = "Port=" + textBox_port.Text + ";";
+            string user = "User Id=" + textBox_user.Text + ";";
+            string pass = "Password=" + textBox_pass.Text + ";";
+            string data = "Database=" + textBox_database.Text + ";";
+            try
+            {
+
+                NpgsqlConnection connection = new NpgsqlConnection(server+port+user+pass+data);
+                connection.Open();
+                textBox_result.Text = "Подключился " + connection.Database;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void autor_btn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string login = logon_txt.Text;
-                string password = password_txt.Text;
-                bool ch = true;
-                var login_pass = from t in bb.Client 
-                            select new { t.login, t.password, t.id_client };
-                foreach (var r in login_pass)
-                {
-                    if (login == r.login && password == r.password)
-                    {
-                        ch = false;
-                        logon_txt.Clear();
-                        password_txt.Clear();
-                        Myprevzakaz _For_Client = new Myprevzakaz(bb.Client.Find(r.id_client));
-                        Hide(); //скрываю форму
-                        _For_Client.ShowDialog();
-                        Close(); 
-                    }
-                }//если это клиент
-                var login_pass1 = from t in bb.Sotrudnik
-                             select new { t.login, t.password, t.id_sotr };
-                foreach (var r in login_pass1)
-                {
-                    if (login == r.login && password == r.password)
-                    {
-                        ch = false;
-                        logon_txt.Clear();
-                        password_txt.Clear();
-                        // открыть форму для сотрудника
-                        //Form_for_client _For_Client = new Form_for_client();
-                        Hide(); //скрываю форму
-                        //_For_Client.ShowDialog();
-                        Show(); //открываем обратно глав форму
-                    }
-                }// если это сотрудник
-
-                if(ch) MessageBox.Show("Неверно введён логин или пароль.");
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Ошибка при обработке данных: " + ex.Message);
-            }
-        }
-
-        private void reg_btn_Click(object sender, EventArgs e)
-        {
-            FormReg reg = new FormReg();
-            Hide();
-            reg.ShowDialog();
-            Form1_Load(reg_btn, e);
-            Show();
-        }//форма для регистрации клиента
     }
 }
